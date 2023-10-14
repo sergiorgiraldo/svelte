98cd9eb567fb4858cd82f95237ca015389061499
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-14 15:55:42 +0200

    feat: slots for button caption ::by sergio giraldo @ 20231014T1555CEST, gpg signed

--- udemy/05-events/src/Meetups/MeetupItem.svelte
+++ udemy/05-events/src/Meetups/MeetupItem.svelte
@@ -35,14 +35,19 @@
 		<p>{description}</p>
 	</div>
 	<footer>
-		<Button href="mailto:{email}" caption="Contact" />
+		<Button href="mailto:{email}">
+			Contact
+		</Button>	
 		<Button
 			mode="outline"
 			color={isFav ? null : "success"}
 			type="button"
-			caption={isFav ? "Unfavorite" : "Favorite"}
-			on:click={() => dispatch("togglefavorite", id)} />
-		<Button type="button" caption="Show Details" />
+			on:click={() => dispatch("togglefavorite", id)}>
+			{isFav ? "Unfavorite" : "Favorite"}
+		</Button>	
+		<Button type="button">
+			Show Details
+		</Button>
 	</footer>
 </article>
 
