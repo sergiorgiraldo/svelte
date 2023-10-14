aaec72261680bd3b4f0921e2daa3a82d1c8eb376
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-14 22:09:18 +0200

    feat: component binding ::by sergio giraldo @ 20231014T2209CEST, gpg signed

---
+++ udemy/06-binding/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import CustomInput from "./CustomInput.svelte";
+  import Toggle from './Toggle.svelte';
+
+  let val = "Max";
+  let selectedOption = 1;
+
+  $: console.log(val);
+  $: console.log(selectedOption);
+
+  // function setValue(event) {
+  //   val = event.target.value;
+  // }
+</script>
+<!-- notice if you have a 2-way binding you cant have dynamic binding at the same time (like something={variable}) -->
+<!-- <input type="text" value={val} on:input={setValue}> -->
+<!-- <input type="text" bind:value={val} /> -->
+<CustomInput bind:val={val} />
+
+<Toggle bind:chosenOption={selectedOption} />
