6bbe6c06d3c1c8efcf106c95f5d9c1e476210c94
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-15 15:05:03 +0200

    feat: component binding: dropdowns ::by sergio giraldo @ 20231015T1505CEST, gpg signed

--- udemy/06-binding/src/App.svelte
+++ udemy/06-binding/src/App.svelte
@@ -1,25 +1,27 @@
 <script>
-  import CustomInput from "./CustomInput.svelte";
-  import Toggle from "./Toggle.svelte";
+	import CustomInput from "./CustomInput.svelte";
+	import Toggle from "./Toggle.svelte";
 
-  let val = "SRG";
-  let price = 0;
-  let selectedOption = 1;
-  let agreed = false;
-  let favColor = 'green';
-  let favColorArr = ['green'];
+	let val = "SRG";
+	let price = 0;
+	let selectedOption = 1;
+	let agreed = false;
+	let favColor = "green";
+	let favColorArr = ["green"];
+	let singleFavColor = "blue";
+	$: console.log(val);
+	$: console.log(selectedOption);
+	$: console.log(price);
+	$: console.log(agreed);
+	$: console.log(favColor);
+	$: console.log(favColorArr);
+	$: console.log(singleFavColor);
 
-  $: console.log(val);
-  $: console.log(selectedOption);
-  $: console.log(price);
-  $: console.log(agreed);
-  $: console.log(favColor);
-  $: console.log(favColorArr);
-
-  // function setValue(event) {
-  //   val = event.target.value;
-  // }
+	// function setValue(event) {
+	//   val = event.target.value;
+	// }
 </script>
+
 <!-- notice if you have a 2-way binding you cant have dynamic binding at the same time (like something={variable}) -->
 
 <!-- <input type="text" value={val} on:input={setValue}> -->
@@ -31,34 +33,45 @@
 <input type="number" bind:value={price} />
 
 <label>
-  <input type="checkbox" bind:checked={agreed} />
-  Agree to terms?
+	<input type="checkbox" bind:checked={agreed} />
+	Agree to terms?
 </label>
 
 <h1>Favorite Color?</h1>
 <label>
-    <input type="radio" name="color1" value="red" bind:group={favColor}>
-    Red
+	<input type="radio" name="color1" value="red" bind:group={favColor} />
+	Red
 </label>
 <label>
-    <input type="radio" name="color1" value="green" bind:group={favColor}>
-    Green
+	<input type="radio" name="color1" value="green" bind:group={favColor} />
+	Green
 </label>
 <label>
-    <input type="radio" name="color1" value="blue" bind:group={favColor}>
-    Blue
+	<input type="radio" name="color1" value="blue" bind:group={favColor} />
+	Blue
 </label>
 
 <h1>Favorite Color<b>S</b>?</h1>
 <label>
-    <input type="checkbox" name="color" value="red" bind:group={favColorArr}>
-    Red
+	<input type="checkbox" name="color" value="red" bind:group={favColorArr} />
+	Red
 </label>
 <label>
-    <input type="checkbox" name="color" value="green" bind:group={favColorArr}>
-    Green
+	<input
+		type="checkbox"
+		name="color"
+		value="green"
+		bind:group={favColorArr} />
+	Green
 </label>
 <label>
-    <input type="checkbox" name="color" value="blue" bind:group={favColorArr}>
-    Blue
+	<input type="checkbox" name="color" value="blue" bind:group={favColorArr} />
+	Blue
 </label>
+
+<h1>Single fav Color</h1>
+<select bind:value={singleFavColor}>
+	<option value="green">Green</option>
+	<option value="red">Red</option>
+	<option value="blue">Blue</option>
+</select>
