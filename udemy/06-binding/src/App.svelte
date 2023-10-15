2449bbc6b05582174286ca59b6ea4120b1ffb005
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-15 15:02:22 +0200

    feat: component binding: radio and checkbox ::by sergio giraldo @ 20231015T1502CEST, gpg signed

--- udemy/06-binding/src/App.svelte
+++ udemy/06-binding/src/App.svelte
@@ -1,20 +1,64 @@
 <script>
   import CustomInput from "./CustomInput.svelte";
-  import Toggle from './Toggle.svelte';
+  import Toggle from "./Toggle.svelte";
 
-  let val = "Max";
+  let val = "SRG";
+  let price = 0;
   let selectedOption = 1;
+  let agreed = false;
+  let favColor = 'green';
+  let favColorArr = ['green'];
 
   $: console.log(val);
   $: console.log(selectedOption);
+  $: console.log(price);
+  $: console.log(agreed);
+  $: console.log(favColor);
+  $: console.log(favColorArr);
 
   // function setValue(event) {
   //   val = event.target.value;
   // }
 </script>
 <!-- notice if you have a 2-way binding you cant have dynamic binding at the same time (like something={variable}) -->
+
 <!-- <input type="text" value={val} on:input={setValue}> -->
 <!-- <input type="text" bind:value={val} /> -->
-<CustomInput bind:val={val} />
+<CustomInput bind:val />
 
 <Toggle bind:chosenOption={selectedOption} />
+
+<input type="number" bind:value={price} />
+
+<label>
+  <input type="checkbox" bind:checked={agreed} />
+  Agree to terms?
+</label>
+
+<h1>Favorite Color?</h1>
+<label>
+    <input type="radio" name="color1" value="red" bind:group={favColor}>
+    Red
+</label>
+<label>
+    <input type="radio" name="color1" value="green" bind:group={favColor}>
+    Green
+</label>
+<label>
+    <input type="radio" name="color1" value="blue" bind:group={favColor}>
+    Blue
+</label>
+
+<h1>Favorite Color<b>S</b>?</h1>
+<label>
+    <input type="checkbox" name="color" value="red" bind:group={favColorArr}>
+    Red
+</label>
+<label>
+    <input type="checkbox" name="color" value="green" bind:group={favColorArr}>
+    Green
+</label>
+<label>
+    <input type="checkbox" name="color" value="blue" bind:group={favColorArr}>
+    Blue
+</label>
