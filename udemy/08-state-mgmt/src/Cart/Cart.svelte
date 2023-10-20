1e743873998f65e4b41e1c4141d5e59410967a6e
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-20 22:10:35 +0200

    feat: shortcut to use a store, use $<name-of-store> ::by sergio giraldo @ 20231020T2210CEST, gpg signed

--- udemy/08-state-mgmt/src/Cart/Cart.svelte
+++ udemy/08-state-mgmt/src/Cart/Cart.svelte
@@ -1,25 +1,12 @@
 <script>
-	import {onDestroy} from "svelte";
 	import cartItems from "./cart-store.js";
 	import CartItem from "./CartItem.svelte";
-
-	let items;
-
-	const unsubscribe = cartItems.subscribe((its) => {
-		items = its;
-	});
-
-	onDestroy(() => {
-		if (unsubscribe){
-			unsubscribe();
-		}
-	});
 </script>
 
 <section>
 	<h1>Cart</h1>
 	<ul>
-		{#each items as item (item.id)}
+		{#each $cartItems as item (item.id)}
 			<CartItem id={item.id} title={item.title} price={item.price} />
 		{:else}
 			<p>No items in cart yet!</p>
