ff0471902397d5cf9bd4834d4b03cfbac2805d3e
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-22 18:26:47 +0200

    feat: animate progress bars ::by sergio giraldo @ 20231022T1826CEST, gpg signed

---
+++ udemy/10-transitions-02-project/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {tweened} from "svelte/motion"
+    import {cubicIn} from "svelte/easing"
+
+    const progress = tweened(0 ,{
+        delay:0,
+        duration:700,
+        easing: cubicIn
+    });
+
+    setTimeout(() => {
+        progress.set(0.5)
+    }, 1500);
+</script>
+
+<progress value={$progress}>/</progress>
