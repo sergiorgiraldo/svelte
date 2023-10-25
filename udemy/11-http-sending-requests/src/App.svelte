e460dbefb419ca5249fe9ca0e23ec79e2937ceda
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-25 16:49:42 +0200

    feat: initial projects using rest api's ::by sergio giraldo @ 20231025T1649CEST, gpg signed

---
+++ udemy/11-http-sending-requests/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	let hobbies = [];
+	let hobbyInput;
+	let isLoading = false;
+
+	function addHobby() {
+		hobbies = [...hobbies, hobbyInput.value];
+
+		isLoading = true;
+		fetch(
+			"https://svelte-001-411ee-default-rtdb.europe-west1.firebasedatabase.app/hobbies.json",
+			{
+				method: "POST",
+				body: JSON.stringify(hobbyInput.value),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			.then((res) => {
+				isLoading = false;
+				if (!res.ok) {
+					throw new Error("Failed!");
+				}
+				alert("Saved Data!");
+			})
+			.catch((err) => {
+				isLoading = false;
+				console.log(err);
+			});
+	}
+</script>
+
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add Hobby</button>
+
+{#if isLoading}
+	<p>Loading...</p>
+{:else}
+	<ul>
+		{#each hobbies as hobby}
+			<li>{hobby}</li>
+		{/each}
+	</ul>
+{/if}
