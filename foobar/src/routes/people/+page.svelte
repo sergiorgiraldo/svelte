c50a5b39ca5601ed4ec6c960c66d0c6d033d700a
Sergio Giraldo <sergio@giraldo.com.br>
2023-11-03 14:50:55 +0100

    feat: sveltekit, with components and fetch data ::by sergio giraldo @ 20231103T1450CET, gpg signed

---
+++ foobar/src/routes/people/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Nice people</title>
+	<meta name="description" content="Some guys" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>People!!!</h1>
+
+	<p>
+		<a href="/people/john">John Doe</a>
+        <br/>
+        Unam incolunt Belgae, aliam Aquitani, tertiam. 
+	</p>
+
+	<p>
+		<a href="/people/jane">Jane Doe</a>
+        <br/>
+        Pellentesque habitant morbi tristique senectus et netus.
+	</p>
+</div>
