2dd4706f649a9252aa9728edbbc8981f8e429233
Sergio Giraldo <sergio@giraldo.com.br>
2023-11-14 09:26:57 +0100

    feat: group similar routes without changing route structure ::by sergio giraldo @ 20231114T0926CET, gpg signed

---
+++ apiroutes/src/routes/company/(marketing)/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let data;
+</script>
+
+<div class="layout">
+	<main>
+		<p>
+			<a href="/">Index</a>
+		</p>
+		<h1>MARKETING PAGES</h1>
+		<slot />
+	</main>
+</div>
+
+<style>
+	@media (min-width: 640px) {
+		.layout {
+			display: grid;
+			gap: 2em;
+			grid-template-columns: 1fr 16em;
+		}
+	}
+</style>
